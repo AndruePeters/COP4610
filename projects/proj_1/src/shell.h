4585a86b4635bb0bb7cd0551c99cd5be97e4051b
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-01-14 15:05:32 -0500

    Added include guards.

--- projects/proj_1/src/shell.h
+++ projects/proj_1/src/shell.h
@@ -1,10 +1,12 @@
 /*
-
+  Rules for evaluating arguments
+  1) Check to see if an alias exists, of so then expand
+  2) Check to see if path exists
 */
 
 
-#ifndef _SHELL_GROUP12_H_
-#define _SHELL_GROUP12_H_
+#ifndef _GROUP12_SHELL_H_
+#define _GROUP12_SHELL_H_
 
 
 
