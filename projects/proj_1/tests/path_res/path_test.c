1407273a6eaa9e0fe3021050a4bb6049f09f97ee
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-01-30 23:01:39 -0500

    Updated test file to reflect api changes

--- projects/proj_1/tests/path_res/path_test.c
+++ projects/proj_1/tests/path_res/path_test.c
@@ -26,9 +26,9 @@ int main()
   printf("res: %s\n", res);
   free(res);
 
-  char* path, *dest = NULL;
-  path = calloc(51, sizeof(char));
-  snprintf(path, 51, "/User/druepeters/./../more_stuff/stuff/../filegc/");
+  char* path;
+  path = calloc(49, sizeof(char));
+  snprintf(path, 49, "User/druepeters/./../more_stuff/stuff/../filec/");
   char* p = get_path(path);
 
 
@@ -38,57 +38,4 @@ int main()
 
 
   return 0;
-}
-
-void print_path_type(enum Path_Type pt)
-{
-  switch(pt) {
-    case PATH_ABS:  printf("PATH_ABS\n"); break;
-    case PATH_HOME: printf("PATH_HOME\n"); break;
-    case PATH_CURR: printf("PATH_CURR\n"); break;
-    case PATH_PREV: printf("PATH_PREV\n"); break;
-    case PATH_PATH: printf("PATH_PATH\n"); break;
-    case PATH_ERR:  printf("PATH_ERR\n"); break;
-    default:        printf("DOESNT EXIST\n"); break;
-  }
-}
-
-void test_get_path()
-{
-  char abso[] = "/asdfjasdfklj/asdfjkasdf";
-  char hom[] = "~/asdfa/sdfasd";
-  char curr1[] = "asdf/asf";
-  char curr2[] = ".asdf";
-  char curr3[] = "./asdf";
-  char prev[] = "../asdf";
-  char sing[] = "s";
-  char blnk[] = "";
-  char* null = NULL;
-
-  printf("%s:\t", abso);
-  print_path_type(get_path_type(abso));
-
-  printf("%s:\t", hom);
-  print_path_type(get_path_type(hom));
-
-  printf("%s:\t", curr1);
-  print_path_type(get_path_type(curr1));
-
-  printf("%s:\t", curr2);
-  print_path_type(get_path_type(curr2));
-
-  printf("%s:\t", curr3);
-  print_path_type(get_path_type(curr3));
-
-  printf("%s:\t", prev);
-  print_path_type(get_path_type(prev));
-
-  printf("%s:\t", sing);
-  print_path_type(get_path_type(sing));
-
-  printf("%s:\t", blnk);
-  print_path_type(get_path_type(blnk));
-
-  printf("%s:\t", null);
-  print_path_type(get_path_type(null));
 }
