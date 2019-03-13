a49b40f9fc39887ae862ebce42a45e767b51ec26
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-03-13 14:37:34 -0400

    Init commit. Consolidated elevator properties from other headers to thsi file.

---
+++ projects/proj_2/part3/my_elev_config.h
@@ -0,0 +1,21 @@
+/*
+  Stores information related to configuration of elevator module.
+*/
+
+#ifndef _GROUP_15_MY_ELEV_CONFIG_H_
+#define _GROUP_15_MY_ELEV_CONFIG_H_
+
+// Pasenger information
+#define MAX_PASSENGERS    (10)
+#define LOAD_SCALE        (2)
+
+// Floor information
+#define BOTTOM_FLOOR      (1)
+#define MAX_FLOOR         (10)
+
+// Elevator Information
+#define MAX_LOAD          (15 * LOAD_SCALE)
+#define TIME_BTW_FLOORS   (2)
+#define TIM_BTW_PASSENGER (1)
+
+#endif
