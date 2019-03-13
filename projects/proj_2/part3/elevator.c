a0232b84e134924f5a39f6211443624ec377f12a
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-03-13 15:22:45 -0400

    Implemented ..up_floor(), ..down_floor(), and began ..unload()

--- projects/proj_2/part3/elevator.c
+++ projects/proj_2/part3/elevator.c
@@ -5,11 +5,14 @@
   Elevator Scheduler
 */
 #include <linux/delay.h>
+#include <linux/types.h>
 #include <elevator.h>
 
 
 
-
+/*
+  Initializes extern struct my_elevator elev to default values.
+*/
 int init_my_elevator(void)
 {
   elev.num_passengers = 0;
@@ -22,7 +25,71 @@ int init_my_elevator(void)
   return 0;
 }
 
+/*
+  Sleeps for a specified amount of time.
+  To be used while loading and unloading passengers and moving floors.
+  time is in seconds.
+*/
 void my_elev_sleep(int time)
 {
   ssleep(time);
+}
+
+/*
+  Moves to elevator to a specific floor.
+  TODO: Finish this.
+*/
+void my_elev_move_to_floor(int floor)
+{
+
+}
+
+/*
+  Moves elevator up a single floor.
+  Does nothing if elevator already at MAX_FLOOR
+*/
+void my_elev_up_floor(void)
+{
+  if (elev.curr_floor == MAX_FLOOR) {
+    return;
+  }
+
+  my_elev_sleep(TIME_BTW_FLOORS);
+  ++elev.curr_floor;
+}
+
+/*
+  Moves elevator down a single floor.
+  Does nothing if elevator is already at MIN_FLOOR
+*/
+void my_elev_down_floor(void)
+{
+  if (elev.curr_floor == MIN_FLOOR) {
+    return;
+  }
+
+  my_elev_sleep(TIME_BTW_FLOORS);
+  --elev.curr_floor;
+}
+
+/*
+  Unloads elevator; essentially by deleting users in a list.
+  Unloads all passengers it can.
+  Works by deleting entries in pass_list;
+*/
+void my_elev_unload(void)
+{
+  struct list_head *pos, *pos_next;
+  list_for_each_safe(pos, pos_next, &elev.pass_list) {
+
+  }
+}
+
+/*
+  Loads as many passengers as possible.
+  Works by moving entries from floors[] to pass_list;
+*/
+void my_elev_load(void)
+{
+
 }
