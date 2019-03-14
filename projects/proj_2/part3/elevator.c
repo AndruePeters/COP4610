d48afcff0d6bd9d88b5cd9731d54df2537174218
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-03-13 20:19:34 -0400

    Added my_elev_load() and my_elev_unload().

--- projects/proj_2/part3/elevator.c
+++ projects/proj_2/part3/elevator.c
@@ -5,6 +5,7 @@
   Elevator Scheduler
 */
 #include <linux/delay.h>
+#include <linux/slab.h>
 #include <linux/types.h>
 #include <elevator.h>
 
@@ -76,20 +77,47 @@ void my_elev_down_floor(void)
   Unloads elevator; essentially by deleting users in a list.
   Unloads all passengers it can.
   Works by deleting entries in pass_list;
+
+  Unloads based upon elev.curr_floor;
 */
 void my_elev_unload(void)
 {
   struct list_head *pos, *pos_next;
-  list_for_each_safe(pos, pos_next, &elev.pass_list) {
+  struct my_elev_passenger *ep  = NULL;
 
+  list_for_each_safe(pos, pos_next, &elev.pass_list) {
+    ep = list_entry(pos, struct my_elev_passenger, list);
+    if (ep->dest_floor == elev.curr_floor) {
+      elev.num_passengers -= my_elev_get_pass_units(ep->pass_type);
+      elev.total_load -= my_elev_get_pass_load(ep->pass_type);
+      list_del(pos);
+      kfree(ep);
+    }
   }
 }
 
 /*
   Loads as many passengers as possible.
   Works by moving entries from floors[] to pass_list;
+
+  Loads based upon elev.curr_floor
 */
 void my_elev_load(void)
 {
+  struct list_head *pos, *pos_next;
+  struct my_elev_passenger *ep = NULL;
+  int pass_units, pass_load;
 
+  list_for_each_safe(pos, pos_next, &(elev.floors[elev.curr_floor - 1].pass_list)) {
+    ep = list_entry(pos, struct my_elev_passenger, list);
+    pass_units = my_elev_get_pass_units(ep->pass_type);
+    pass_load = my_elev_get_pass_load(ep->pass_type);
+
+    if ((pass_units + elev.num_passengers <= MAX_PASSENGERS) &&
+        (pass_load + elev.total_load <= MAX_LOAD))             {
+          list_move_tail(pos, &elev.pass_list);
+          elev.num_passengers += pass_units;
+          elev.total_load += pass_load;
+    }
+  }
 }
