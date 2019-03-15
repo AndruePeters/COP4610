3e2dbacece854849efc8746b9cd08afac4915a20
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-03-15 08:15:42 -0400

    init commit

---
+++ projects/proj_2/part3/sys_call/sys_my_elev.c
@@ -0,0 +1,44 @@
+/*
+	Andrue Peters
+	3/10/19
+	System call module for elevator scheduler.
+*/
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* System call */
+int (*start_elevator)(void) = NULL;
+int (*stop_elevator)(void) = NULL;
+int (*issue_request)(int pass_type, int start_floor, int dest_floor) = NULL;
+
+EXPORT_SYMBOL(start_elevator);
+EXPORT_SYMBOL(stop_elevator);
+EXPORT_SYMBOL(issue_request);
+
+asmlinkage int sys_start_elevator() 
+{
+	if (start_elevator != NULL) {
+		return start_elevator();
+	} else {
+		return -ENOSYS;
+	}
+}
+
+asmlinkage int sys_stop_elevator()
+{
+	if (stop_elevator != NULL) {
+		return stop_elevator();
+	} else {
+		return -ENOSYS;
+	}
+}
+ 
+asmlinkage int sys_issue_request(int pass_type, int start_floor, int dest_floor) 
+{
+	if (issue_request != NULL) {
+		return issue_request(pass_type, start_floor, dest_floor);
+	} else {
+		return -ENOSYS;
+	}
+}
