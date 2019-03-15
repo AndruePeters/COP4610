f22a9765cc3ab375217cabdbfd961dd87022dbbd
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-03-15 10:37:41 -0400

    Debug methods

--- projects/proj_2/part3/elevator.c
+++ projects/proj_2/part3/elevator.c
@@ -308,6 +308,7 @@ const char* my_elev_state_char(struct my_elevator *elev)
   char *state;
   printk(KERN_WARNING "Entered my_elev_state_char\n");
   if (mutex_lock_interruptible(&(elev->mtx)) == 0) {
+    printk(KERN_WARNING "Got my_elev_state_char lock\n");
     switch(elev->state) {
       case MY_ELEV_OFFLINE: state = "OFFLINE"; break;
       case MY_ELEV_IDLE: state = "IDLE"; break;
