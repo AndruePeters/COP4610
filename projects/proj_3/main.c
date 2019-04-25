6b1d1e52ed45366fad4f79c9a2d736f371f9a47e
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-04-25 15:04:37 -0400

    Test code shows byte order is correct

--- projects/proj_3/main.c
+++ projects/proj_3/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -12,6 +11,10 @@ int main()
   uint8_t *buffer;
   long filelen;
   int i;
+  struct fat_bpb bpb;
+  uint8_t *b_ptr = (char *)&bpb;
+
+
   fileptr = fopen("fat32.img", "rb");
   fseek(fileptr, 0, SEEK_END);
   filelen = ftell(fileptr);
@@ -19,13 +22,23 @@ int main()
 
   buffer = (uint8_t *)malloc( (filelen+1) * sizeof(char));
   fread(buffer, filelen, 1, fileptr);
+  rewind(fileptr);
+  load_fat_bpb(&bpb, fileptr);
   fclose(fileptr);
 
   for (i = 0; i < 50; ++i) {
     printf("0x%02x  ", buffer[i]);
     if (i % 5 == 0) printf("\n");
   }
+  printf("\n\n");
 
+  for (i = 0; i < 50; ++i) {
+    printf("0x%02x  ", b_ptr[i]);
+    if (i % 5 == 0) printf("\n");
+  }
 printf("Size of FAT32_BPB:\t%lu\nSize of FAT32_DIR:\t%lu\n", sizeof(struct fat_bpb), sizeof(struct fat_dir));
+printf("\n\n");
+dump_fat_bpb(&bpb);
+
   return 0;
 }
