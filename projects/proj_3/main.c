0e45e7e7c48ef015795fc8d52a1d219fd11fd39d
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-04-26 16:33:35 -0400

    Main now prints information for "Red" directory and lists all folders in Red

--- projects/proj_3/main.c
+++ projects/proj_3/main.c
@@ -2,10 +2,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "fat32.h"
 #include "fat32_masks.h"
-
 
 int main()
 {
@@ -25,10 +23,9 @@ int main()
   rewind(fileptr);
   load_fat_bpb(&bpb, fileptr);
 
-  dump_fat_bpb(&bpb);
   root_dir = first_sect_of_clus(&bpb, 2);
   load_fat_dir(&bpb, &d, fileptr, 2, 2);
-  dump_fat_dir(&d);
+
 
   for( i = 0; i < 16; ++i) {
     load_fat_dir(&bpb, &d, fileptr, 2, i);
@@ -39,19 +36,19 @@ int main()
         red = d;
       }
     }
-
-  for (i = 0; i < 16; ++i) {
-
   }
 
-
+  uint32_t addr = 0x1b4;
+  printf("addr: %#x\n", fat_get_next_clus(&bpb, fileptr, addr));
+  while ( addr != 0xFFFFFFF) {
+    for (i = 0; i < 16; ++i) {
+      load_fat_dir(&bpb, &d, fileptr, addr, i);
+      if (d.DIR_Attr & ATTR_DIRECTORY)
+        printf("Dir_name: %.11s\n", d.DIR_Name);
+    }
+    addr = fat_get_next_clus(&bpb, fileptr, addr);
   }
-  red_clust = (red.DIR_FstClusHI << 16) | (red.DIR_FstClusLO);
-  printf("Red cluster: %#x\n", red_clust);
 
-  printf("getFatAddress(0x1bf): %#x\n", getFatAddressByCluster(&bpb, 0x21d));
-  printf("getFatEntry(0x1bf): %#x\n", getFatEntry(&bpb, 0x21d, fileptr));
-  printf("Byte entry : %#x\n", cluster_to_byte(&bpb, fat_get_next_clus(&bpb, fileptr, 0x1f9)));
 
 
   return 0;
