b52fcee5c43e7063eef703e02681fe99af68e059
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-04-26 15:29:41 -0400

    Test code to calculate next address and cluster

--- projects/proj_3/main.c
+++ projects/proj_3/main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "fat32.h"
 #include "fat32_masks.h"
 
+
 int main()
 {
   FILE *fileptr;
-  uint8_t *buffer;
-  long filelen;
   int i;
   struct fat_bpb bpb;
   uint8_t *b_ptr = (char *)&bpb;
   unsigned root_dir;
   struct fat_dir d, red;
+  uint32_t red_clust;
 
   fileptr = fopen("fat32.img", "rb");
   fseek(fileptr, 0, SEEK_END);
-  filelen = ftell(fileptr);
   rewind(fileptr);
 
-  buffer = (uint8_t *)malloc( (filelen+1) * sizeof(char));
-  fread(buffer, filelen, 1, fileptr);
+
   rewind(fileptr);
   load_fat_bpb(&bpb, fileptr);
 
@@ -33,13 +32,26 @@ int main()
 
   for( i = 0; i < 16; ++i) {
     load_fat_dir(&bpb, &d, fileptr, 2, i);
-    if (d.DIR_Attr & ATTR_DIRECTORY)
-      printf("Dir_name: %.11s\n", d.DIR_Name);
+    if (d.DIR_Attr & ATTR_DIRECTORY) {
+      if (strncmp(d.DIR_Name, "RED", 3) == 0) {
+        printf("Dir_name: %.11s\n", d.DIR_Name);
+        dump_fat_dir(&d);
+        red = d;
+      }
+    }
+
+  for (i = 0; i < 16; ++i) {
+
   }
 
 
-  printf("First data sector: 0x%x\n", first_data_sector(&bpb));
-  printf("Root dir sector: %x\n", cluster_to_byte(&bpb, 3));
+  }
+  red_clust = (red.DIR_FstClusHI << 16) | (red.DIR_FstClusLO);
+  printf("Red cluster: %#x\n", red_clust);
+
+  printf("getFatAddress(0x1bf): %#x\n", getFatAddressByCluster(&bpb, 0x21d));
+  printf("getFatEntry(0x1bf): %#x\n", getFatEntry(&bpb, 0x21d, fileptr));
+  printf("Byte entry : %#x\n", cluster_to_byte(&bpb, fat_get_next_clus(&bpb, fileptr, 0x1f9)));
 
 
   return 0;
